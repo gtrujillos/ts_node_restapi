c48cc1d1e038968162d1de02055ff523743ae31e
Gonzalo Trujillo Silva <gtrujillos@gmail.com>
2019-11-13 20:45:51 -0500

    1.0.45

--- dist/config/jwt.js
+++ dist/config/jwt.js
@@ -1,1 +1,1 @@
-var a195_0x5de1=['exports','Lz+d=Mv#rL3pCPaAch=&MwbR9v!We*_X#gx_Wm%%db!&yKSMm+'];(function(_0x3b3b0e,_0x366977){var _0xaed733=function(_0x2647a1){while(--_0x2647a1){_0x3b3b0e['push'](_0x3b3b0e['shift']());}};_0xaed733(++_0x366977);}(a195_0x5de1,0x8c));var a195_0x3b46=function(_0x143512,_0x58272a){_0x143512=_0x143512-0x0;var _0x2f8bc2=a195_0x5de1[_0x143512];return _0x2f8bc2;};module[a195_0x3b46('0x0')]={'secret':a195_0x3b46('0x1'),'options':{}};
+module['exports']={'secret':'Lz+d=Mv#rL3pCPaAch=&MwbR9v!We*_X#gx_Wm%%db!&yKSMm+','options':{}};
